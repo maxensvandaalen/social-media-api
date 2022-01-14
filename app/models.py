abd9aec3741b6cfc3218580fc371f1e09676b764
maxensvandaalen <47213605+maxensvandaalen@users.noreply.github.com>
2022-01-14 16:25:59 +0800

    corrected formatting

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Boolean, Column, Integer, String
 from .database import Base
 
+
 class Post(Base):
     __tablename__ = "posts"
 
--- app/schemas.py
+++ app/schemas.py
@@ -1,15 +1,18 @@
 from pydantic import BaseModel
 
+
 class PostBase(BaseModel):
     title: str
     content: str
     is_published: bool = True
 
+
 class PostCreate(PostBase):
     pass
 
+
 class Post(PostBase):
     id: int
-    
+
     class Config:
         orm_mode = True
