819c1466b2f7dfba9ce3316fa2c5097d11149af9
maxensvandaalen <47213605+maxensvandaalen@users.noreply.github.com>
2022-01-14 14:45:56 +0800

    add posts table + alembic migration

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from xmlrpc.client import Boolean
+from sqlalchemy import Boolean, Column, Integer, String
+from .database import Base
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    is_published = Column(Boolean, default=True)
