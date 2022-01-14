93fd4759ed40ed3b8dae526315d3227433e33b0e
maxensvandaalen <47213605+maxensvandaalen@users.noreply.github.com>
2022-01-14 16:19:17 +0800

    add pydantic models for posts

---
+++ app/schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    is_published: bool = True
+
+class PostCreate(PostBase):
+    pass
+
+class Post(PostBase):
+    id: int
+    
+    class Config:
+        orm_mode = True
