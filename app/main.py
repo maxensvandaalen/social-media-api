461af4ec8a33a2b4a5d4a75ff4791de9740a07e1
maxensvandaalen <47213605+maxensvandaalen@users.noreply.github.com>
2022-01-14 16:48:21 +0800

    add first call to read posts

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+
+from .database import get_db
+from . import models, schemas
 
 app = FastAPI()
 
@@ -6,3 +10,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/posts/", response_model=list[schemas.Post])
+def read_posts(db: Session = Depends(get_db)):
+    return db.query(models.Post).all()
