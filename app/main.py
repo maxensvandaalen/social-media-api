cf9b373da4572eafdd16ef7e0cc539e436e29dc1
maxensvandaalen <47213605+maxensvandaalen@users.noreply.github.com>
2022-01-18 11:26:39 +0800

    created routers and moved posts

--- app/main.py
+++ app/main.py
@@ -1,17 +1,10 @@
-from fastapi import Depends, FastAPI
-from sqlalchemy.orm import Session
-
-from .database import get_db
-from . import models, schemas
+from fastapi import FastAPI
+from .routers import post
 
 app = FastAPI()
+app.include_router(post.router)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.get("/posts/", response_model=list[schemas.Post])
-def read_posts(db: Session = Depends(get_db)):
-    return db.query(models.Post).all()
---
+++ app/routers/post.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from .. import models, schemas
+from ..database import get_db
+
+
+router = APIRouter(prefix="/posts", tags=["Posts"])
+
+
+@router.get("/posts/", response_model=list[schemas.Post])
+def read_posts(db: Session = Depends(get_db)):
+    return db.query(models.Post).all()
