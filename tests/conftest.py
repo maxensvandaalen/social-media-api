0be67082373bef7554b8fbd425fa21bf74aba42d
maxensvandaalen <47213605+maxensvandaalen@users.noreply.github.com>
2022-01-21 14:19:56 +0800

    introduce test db for unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,33 @@
 from fastapi.testclient import TestClient
-import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from pytest import fixture
+from app.config import settings
 from app.main import app
+from app.database import Base, get_db
+
+testdatabase = f"{settings.SQLALCHEMY_DATABASE_URL}_test"
+
+engine = create_engine(testdatabase)
+
+TestingSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
 
 
-@pytest.fixture
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
+
+
+@fixture
 def client():
     yield TestClient(app)
--- tests/test_post.py
+++ tests/test_post.py
@@ -6,4 +6,3 @@ def test_get_all_posts(client):
 def test_get_post_by_id(client):
     response = client.get("/posts/1")
     assert response.status_code == 200
-    
