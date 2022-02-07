be7bd536bb8e14d9beb84582d55f325fe690c7db
maxensvandaalen <47213605+maxensvandaalen@users.noreply.github.com>
2022-02-07 15:02:19 +0800

    feat: return 404 if post id not exists

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from .. import models, schemas
 from ..database import get_db
@@ -12,6 +12,12 @@ def read_posts(db: Session = Depends(get_db)):
     return db.query(models.Post).all()
 
 
-@router.get("/{post_id}", response_model=schemas.Post )
+@router.get("/{post_id}", response_model=schemas.Post)
 def read_post_by_id(post_id: int, db: Session = Depends(get_db)):
-    return db.query(models.Post).filter(models.Post.id == post_id).first()
+    post = db.query(models.Post).filter(models.Post.id == post_id).first()
+
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {post_id} does not exist")
+
+    return post
--- tests/test_post.py
+++ tests/test_post.py
@@ -9,3 +9,9 @@ def test_get_post_by_id(create_test_post, client):
     data = response.json()
     assert data["title"] == "Some title"
     assert data["content"] == "Some content"
+
+
+def test_get_post_non_exist(create_test_post, client):
+    response = client.get("/posts/12345")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "post with id: 12345 does not exist"}
