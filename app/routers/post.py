afa016d30a1f72e5168a732b18f2be26bcf91f05
maxensvandaalen <47213605+maxensvandaalen@users.noreply.github.com>
2022-02-07 16:55:34 +0800

    feat: add create post

--- app/routers/post.py
+++ app/routers/post.py
@@ -21,3 +21,15 @@ def read_post_by_id(post_id: int, db: Session = Depends(get_db)):
                             detail=f"post with id: {post_id} does not exist")
 
     return post
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_post(post: schemas.PostCreate, user_id: int, db: Session = Depends(get_db)):
+    db_post = models.Post(
+        owner_id=user_id, **post.dict())
+    db.add(db_post)
+    db.commit()
+    db.refresh(db_post)
+    return db_post
+
+# TODO create post, delete post, update post
--- tests/test_post.py
+++ tests/test_post.py
@@ -15,3 +15,14 @@ def test_get_post_non_exist(create_test_post, client):
     response = client.get("/posts/12345")
     assert response.status_code == 404
     assert response.json() == {"detail": "post with id: 12345 does not exist"}
+
+
+def test_create_post(create_test_user, client):
+    response = client.post(
+        "/posts/?user_id=1",
+        json={"title": "a title", "content": "some content"}
+    )
+    assert response.status_code == 201
+    data = response.json()
+    assert data["title"] == "a title"
+    assert data["content"] == "some content"
