6be55492897c8b50793f898083c09bcd6f0f5abe
maxensvandaalen <47213605+maxensvandaalen@users.noreply.github.com>
2022-02-11 14:35:00 +0800

    feat: new user creation does not allow same name or email

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from psycopg2.errors import UniqueViolation
 from sqlalchemy.orm import Session
 from .. import models, schemas
 from ..database import get_db
@@ -10,9 +11,18 @@ router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.post("/", status_code = status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    user_names = db.query(models.User.name).all()
+    user_emails = db.query(models.User.email).all()
+
+    if user.name in [value for value, in user_names]:
+        raise HTTPException(status_code=409, detail="this username is already registered")
+    
+    if user.email in [value for value, in user_emails]:
+        raise HTTPException(status_code=409, detail="this emailadress is already registered")
+
     hashed_password = get_password_hash(user.password)
     user.password = hashed_password
-    new_user = models.User(**user.dict())
+    new_user = models.User(**user.dict())       
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
--- tests/test_user.py
+++ tests/test_user.py
@@ -11,3 +11,24 @@ def test_create_user(client):
     data = response.json()
     assert data["name"] == "JohnDoe"
     assert data["email"] == "john_doe@doe.com"
+
+
+def test_create_user_existing_username(create_test_user, client):
+    response = client.post(
+        "/users/",
+        json={"name": "John Doe", "email": "other@email.com",
+              "password": "password456"}
+    )
+    assert response.status_code == 409
+    assert response.json() == {"detail": "this username is already registered"}
+
+
+def test_create_user_existing_email(create_test_user, client):
+    response = client.post(
+        "/users/",
+        json={"name": "OtherUserName", "email": "john_doe@gmail.com",
+              "password": "password123"}
+    )
+    assert response.status_code == 409
+    assert response.json() == {
+        "detail": "this emailadress is already registered"}
