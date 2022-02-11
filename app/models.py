80fe491ad09e1adc18568ed108c020076223f02d
maxensvandaalen <47213605+maxensvandaalen@users.noreply.github.com>
2022-02-11 13:36:56 +0800

    fix: add unique constraint for username + created_at column for users and comments tables

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, TIMESTAMP
+from sqlalchemy.sql.expression import text
 from sqlalchemy.orm import relationship
-from sqlalchemy.sql import func
 from .database import Base
 
 
@@ -11,7 +11,7 @@ class Post(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     is_published = Column(Boolean, default=True, nullable=False)
-    created_at = Column(TIMESTAMP, server_default=func.now())
+    created_at = Column(TIMESTAMP, server_default=text("timezone('utc', now())"))
     owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     
     owner = relationship("User")
@@ -20,10 +20,10 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
+    name = Column(String, nullable=False, unique=True)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
-    
+    created_at = Column(TIMESTAMP, server_default=text("timezone('utc', now())"))
 
 class Comment(Base):
     __tablename__ = "comments"
@@ -32,3 +32,4 @@ class Comment(Base):
     content = Column(String, nullable=False)
     post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), nullable=False)
     owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    created_at = Column(TIMESTAMP, server_default=text("timezone('utc', now())"))
